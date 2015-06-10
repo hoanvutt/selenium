bd1ac41b8d7d5eb03dcc44c55cc38d050e01badf
z.tran.hoanvu <z.tran.hoanvu@gree.net>
2015-06-10 11:30:38 +0700

    Add Chrome Driver

--- Learning/src/com/packt/webdriver/chapter1/NavagateToAUrl.java
+++ Learning/src/com/packt/webdriver/chapter1/NavagateToAUrl.java
@@ -1,11 +1,13 @@
 package com.packt.webdriver.chapter1;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class NavagateToAUrl {
 	public static void main(String[] args){
-		WebDriver driver = new FirefoxDriver();
+		// For Chrome Driver on iMac -> Download on http://chromedriver.storage.googleapis.com/index.html
+		System.setProperty("webdriver.chrome.driver", "/Users/vuth/selenium-2.46.0/chromedriver");
+		WebDriver driver = new ChromeDriver();
 		driver.get("http://google.com.vn");
 	}
 }
